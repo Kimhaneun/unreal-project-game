70aafac8975f5d8759964f2293c14fbecb0fa896
HanYi <Kimhaneun070929@gmail.com>
2025-03-18 11:51:04 +0900

    feat: input system

--- Project/Source/Project/Player/Project0PlayerController.cpp
+++ Project/Source/Project/Player/Project0PlayerController.cpp
@@ -2,4 +2,60 @@
 
 
 #include "Player/Project0PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AProject0PlayerController::AProject0PlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void AProject0PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+}
+
+void AProject0PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	auto* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &ThisClass::InputAttack);
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &ThisClass::InputMovement);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ThisClass::InputTurn);
+	}
+}
+
+void AProject0PlayerController::InputAttack(const FInputActionValue& InputValue)
+{
+	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor::Blue, TEXT("Attack"));
+}
+
+void AProject0PlayerController::InputMovement(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	if (MovementVector.X != 0)
+	{
+		FVector Direction = GetPawn()->GetActorForwardVector() * MovementVector.X;
+		GetPawn()->AddActorWorldOffset(Direction);
+	}
+
+	if (MovementVector.Y != 0)
+	{
+		FVector Direction = GetPawn()->GetActorRightVector() * MovementVector.Y;
+		GetPawn()->AddActorWorldOffset(Direction);
+	}
+}
+
+void AProject0PlayerController::InputTurn(const FInputActionValue& InputValue)
+{
+	float XValue = InputValue.Get<float>();
+	FRotator Rotation(0.0f, XValue, 0.0f);
+	GetPawn()->AddActorLocalRotation(Rotation);
+}
--- Project/Source/Project/Player/Project0PlayerController.h
+++ Project/Source/Project/Player/Project0PlayerController.h
@@ -6,12 +6,38 @@
 #include "GameFramework/PlayerController.h"
 #include "Project0PlayerController.generated.h"
 
+struct FInputActionValue;
+
 /**
- * 
+ *
  */
 UCLASS()
 class PROJECT_API AProject0PlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AProject0PlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void InputAttack(const FInputActionValue& InputValue);
+	void InputMovement(const FInputActionValue& InputValue);
+	void InputTurn(const FInputActionValue& InputValue);
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> AttackAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> TurnAction;
 };
